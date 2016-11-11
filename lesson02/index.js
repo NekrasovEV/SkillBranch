03b75f3a0c83aa1aa2fceb2ffaf9f212699b9360
Evgeniy <NekrasovEV@mail.ru>
2016-11-11 19:13:19 +0300

    test2

--- lesson02/index.js
+++ lesson02/index.js
@@ -16,6 +16,52 @@ app.get('/', function (req, res) {
 });
 
 
+app.get('/task2b', function (req, res) {
+	let ret = "Invalid fullname";
+
+	let {fullname = ''} = req.query;
+
+	fullname = fullname.toLowerCase();
+	if(/\d|_|\/|\[|\\|\^|\$|\.|\||\?|\*|\+|\(|\)|\./.test(fullname))
+	{
+		return res.send(ret);
+	}
+
+	let reg = fullname.match(/([^\s]+\s*)/ig) || [];
+
+	let FIO = reg.reverse().filter((el)=>el);
+
+	switch(FIO.length){
+		case 3 : {
+			ret = `${ucfirst(FIO[0])} ${ucfirst(FIO[2][0])}. ${ucfirst(FIO[1][0])}.`;
+			break;
+		}
+		case 2 : {
+			ret = `${ucfirst(FIO[0])} ${ucfirst(FIO[1][0])}.`;
+			break;
+		}
+		case 1 : {
+			ret = `${ucfirst(FIO[0])}`;
+			break;
+		}
+	}
+
+	res.send(ret);
+});
+
+function ucfirst(str)
+{
+	var first = str.charAt(0).toUpperCase();
+	return first + str.substr(1);
+}
+
+
+
+
+
+
+
+
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });
--- test.js
+++ test.js
@@ -1,2 +1,28 @@
-let a = parseInt("aaa",8);
-console.log(a+1);
+
+
+let fullName = "Евгений Владимирович  Путин некрасов";
+
+let reg = fullName.match(/([\wа-яё]+\s*)/ig);
+
+let FIO = reg.reverse().filter((el)=>el);
+
+let ret;
+
+switch(FIO.length){
+	case 3 : {
+		ret = `${FIO[0]} ${FIO[2][0]}. ${FIO[1][0]}.`;
+		break;
+	}
+	case 2 : {
+		ret = `${FIO[0]} ${FIO[1][0]}.`;
+		break;
+	}
+	case 1 : {
+		ret = `${FIO[0]}`;
+		break;
+	}
+	default :
+		ret = "Invalid fullname";
+}
+
+console.log(ret);
