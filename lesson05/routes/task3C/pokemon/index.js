8deced1775918bc6d3e53415c0ddb62e553e8897
evgeny <nekrasovEV@mail.ru>
2016-11-25 10:06:31 +0300

    init task3B

--- lesson05/routes/task3C/pokemon/index.js
+++ lesson05/routes/task3C/pokemon/index.js
@@ -7,17 +7,7 @@ module.exports = {
 		let ret;
 		let sorting = [...data];
 
-		let sort = sorting.sort((pokemon1,pokemon2 ) => {
-			let ret;
-
-			if((pokemon1.name) > (pokemon2.name))
-				return 1
-			else if((pokemon1.name) < (pokemon2.name))
-				return -1
-			else
-				return 0;
-		})
-
+		let sort = _.orderBy(sorting, ['name'], ['asc']);
 
 		return await res.json(limit(sort.map((pokemon)=>pokemon.name), query));
 	},
@@ -27,36 +17,21 @@ module.exports = {
 		let ret;
 		let sorting = [...data];
 
-		let sort = sorting.sort((pokemon1,pokemon2 ) => {
-			let ret;
-
-			if((pokemon1.weight / pokemon1.height) > (pokemon2.weight / pokemon2.height))
-				return 1
-			else if((pokemon1.weight / pokemon1.height) < (pokemon2.weight / pokemon2.height))
-				return -1
-			else
-				return 0;
-		})
+		let sort = _.orderBy(sorting, [(item) => {
+			return item.weight/item.height;
+		}, 'name'], ['desc', 'asc']);
 
 
-		return await res.json(limit(sort.reverse().map((pokemon)=>pokemon.name), query));
+		return await res.json(limit(sort.map((pokemon)=>pokemon.name), query));
 	},
 	angular : async function (req, res) {
 		let {query, params, data} = req;
 		let ret;
 		let sorting = [...data];
 
-		let sort = sorting.sort((pokemon1,pokemon2 ) => {
-			let ret;
-
-			if((pokemon1.weight / pokemon1.height) > (pokemon2.weight / pokemon2.height))
-				return 1
-			else if((pokemon1.weight / pokemon1.height) < (pokemon2.weight / pokemon2.height))
-				return -1
-			else
-				return 0;
-		})
-
+		let sort = _.orderBy(sorting, (item) => {
+			return [item.weight/item.height, item.name];
+		}, ['asc','asc']);
 
 		return await res.json(limit(sort.map((pokemon)=>pokemon.name), query));
 	},
@@ -65,16 +40,16 @@ module.exports = {
 		let ret;
 		let sorting = [...data];
 
-		let sort = _.sortBy(sorting, 'height').reverse();
+		let sort = _.orderBy(sorting, ['height', 'name'], ['desc','asc']);
 
-		return await res.json(_.sortBy(limit(sort.map((pokemon)=>pokemon.name), query),['height','name']));
+		return await res.json(limit(sort.map((pokemon)=>pokemon.name), query));
 	},
 	micro : async function (req, res) {
 		let {query, params, data} = req;
 		let ret;
 		let sorting = [...data];
 
-		let sort = _.sortBy(sorting, 'height');
+		let sort = _.orderBy(sorting, ['height', 'name'], ['asc','asc']);
 
 
 		return await res.json(limit(sort.map((pokemon)=>pokemon.name), query));
@@ -83,24 +58,23 @@ module.exports = {
 		let {query, params, data} = req;
 		let ret;
 		let sorting = [...data];
+		let sort = _.orderBy(sorting, ['weight', 'name'], ['desc','asc']);
 
-		let sort = _.sortBy(sorting, 'weight');
-
-		return await res.json(limit(sort.reverse().map((pokemon)=>pokemon.name), query));
+		return await res.json(limit(sort.map((pokemon)=>pokemon.name), query));
 	},
 	light : async function (req, res) {
 		let {query, params, data} = req;
 		let ret;
 		let sorting = [...data];
 
-		let sort = _.sortBy(sorting, 'weight');
+		let sort = _.orderBy(sorting, ['weight', 'name'], ['asc','asc']);
 
 		return await res.json(limit(sort.map((pokemon)=>pokemon.name), query));
 	}
 }
 
 function limit(data, {offset = 0, limit = 20}){
-	return data.filter((el) => el).slice(offset, limit);
+	return data.filter((el) => el).slice(offset).slice(0,limit);
 }
 
 
